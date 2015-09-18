191f1eee393baae4a55f98285fd3d83db616bc27
root <root@esmir>
2015-09-18 08:36:41 -0430

    Subiendo login e interfaz

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,12 +1,44 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html>
 <html>
+    
+      <style type="text/css">
+            body {
+                background-image: url("<?php echo Yii::app()->baseUrl; ?>/images/a.jpg")  !important;
+                background-color: White !important;
+                height: 100% !important;
+                width: 100% !important;
+                background-size: cover !important;
+                /*background-repeat: no-repeat !important;*/
+                /*background-position: right bottom  !important;*/
+                /*background-attachment:fixed !important;*/
+            }
+            .navbar-default .nav > li>a, .navbar-default .nav>li>a:focus {
+                color: rgba(255,255,255,.7) !important;
+            }
+            .navbar-default .navbar-nav>li>a {
+                color: Black !important;
+            }
+            .navbar-default.affix .nav > li>a, .navbar-default.affix .nav>li>a:focus {
+                color: #222 !important;
+            }
+            .navbar-default {
+                background-color: rgba(255,255,255,.3) !important; 
+                border-color: LightSkyBlue !important;
+            }
+            .navbar-default.affix {
+                border-color: rgba(34,34,34,.05) !important;
+                background-color: #fff !important;
+            }
+
+        </style>
+    
+    
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="language" content="en">
 
 	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
 	<!--[if lt IE 8]>
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
@@ -33,74 +65,45 @@
 			),
 		));*/ 
 
-
+    
+    
     $this->widget(
-        'booster.widgets.TbNavbar',
-        array(
-            'type' => null, // null or 'inverse'
-            'brand' => CHtml::encode(Yii::app()->name),
-            'brandUrl' => '#',
-            'collapse' => true, // requires bootstrap-responsive.css
-            'fixed' => false,
-            'fluid' => true,
-            'items' => array(
-                array(
-                    'class' => 'booster.widgets.TbMenu',
-                	'type' => 'navbar',
-                    'items' => array(
-                        array('label' => 'Inicio', 'url' => '#', 'active' => true),
-                        array('label' => 'Twee', 'url' => array('/tweet/index')),
-                        array(
-                            'label' => 'Dropdown',
-                            'url' => '#',
-                            'items' => array(
-                                array('label' => 'Action', 'url' => '#'),
-                                array('label' => 'Another action', 'url' => '#'),
-                                array(
-                                    'label' => 'Something else here',
-                                    'url' => '#'
-                                ),
-                                '---',
-                                array('label' => 'NAV HEADER'),
-                                array('label' => 'Separated link', 'url' => '#'),
-                                array(
-                                    'label' => 'One more separated link',
-                                    'url' => '#'
-                                ),
-                            )
-                        ),
-                    ),
-                ),
-                '<form class="navbar-form navbar-left" action=""><div class="form-group"><input type="text" class="form-control" placeholder="Search"></div></form>',
-                array(
-                    'class' => 'booster.widgets.TbMenu',
-                    'type' => 'navbar',
-                    'htmlOptions' => array('class' => 'pull-right'),
-                    'items' => array(
-                        array('label' => 
-				//Yii::app()->user->foto_perfil." ".
-				//Yii::app()->user->usuario, 
-"aqui",'url' => '#'),
-                        '---',
-                        array(
-                            'label' => 'Dropdown',
-                            'url' => '#',
-                            'items' => array(
-                                array('label' => 'Action', 'url' => '#'),
-                                array('label' => 'Another action', 'url' => '#'),
-                                array(
-                                    'label' => 'Something else here',
-                                    'url' => '#'
-                                ),
-                                '---',
-                                array('label' => 'Separated link', 'url' => '#'),
-                            )
-                        ),
-                    ),
+    'booster.widgets.TbNavbar',
+    array(
+        'type' => 'inverse',
+        'brand' => CHtml::encode(Yii::app()->name),
+        'brandUrl' => '#',
+        'collapse' => true, // requires bootstrap-responsive.css
+        'fixed' => false,
+        'fluid' => true,
+        'items' => array(
+            array(
+                'class' => 'booster.widgets.TbMenu',
+                'type' => 'navbar',
+                'items' => array(
+//                    array('label' => 'Inicio', 'url' => '#', 'active' => true),
+                    array('label' => 'Inicio', 'url' => array('/tweet/index')),
+                    array('label' => 'Usuarios', 'url' => array('/usuario/index')),
+                    
                 ),
             ),
-        )
-    );
+//            '<form class="navbar-form navbar-left" action=""><div class="form-group"><input type="text" class="form-control" placeholder="Search"></div></form>',
+            array(
+                'class' => 'booster.widgets.TbMenu',
+                'type' => 'navbar',
+                'htmlOptions' => array('class' => 'pull-right'),
+                'items' => array(
+//                    array('label' => //Yii::app()->user->foto_perfil." ".
+////				Yii::app()->user->usuario, 
+//                                "aqui",'url' => '#'),
+                    '---',
+                    array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+                ),
+            ),
+        ),
+    )
+);
+    
 
 /*
 echo CHtml::image(Yii::app()->baseUrl."/images/".Yii::app()->user->getState('foto_perfil'),"texto de alt",              array("height"=>"100", "width"=>"100")  )
@@ -115,14 +118,9 @@ echo CHtml::image(Yii::app()->baseUrl."/images/".Yii::app()->user->getState('fot
 
 	<?php echo $content; ?>
 
-	<div class="clear"></div>
+	
 
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
-
+	
 </div><!-- page -->
 
 </body>
